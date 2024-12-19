2b8694ba79cf4f5daa25bd5b9678374cf3588b06
qinfyy <qinfyy233@gmail.com>
2024-12-19 23:21:39 +0800

    添加项目文件。

---
+++ Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(501, 100)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(150, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "开始合并"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(36, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(77, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "客户端路径："
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(119, 46)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(376, 21)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(119, 18)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(376, 21)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(101, 12)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "游戏差分包路径："
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(119, 73)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(376, 21)
+        Me.TextBox3.TabIndex = 6
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(14, 131)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox4.Size = New System.Drawing.Size(637, 287)
+        Me.TextBox4.TabIndex = 8
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(636, 76)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(15, 14)
+        Me.CheckBox1.TabIndex = 9
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(501, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(46, 20)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "选择"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(501, 47)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(46, 20)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "选择"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(501, 74)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(46, 20)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "选择"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(101, 12)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "语音差分包路径："
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(553, 47)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(77, 20)
+        Me.Button5.TabIndex = 14
+        Me.Button5.Text = "选择压缩包"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(553, 72)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(77, 20)
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "选择压缩包"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 116)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 12)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "日志："
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(664, 434)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "合并差分包"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,301 @@
+﻿Imports System.IO
+Imports System.Text.Json
+Imports System.Text.Json.Nodes
+Imports SharpCompress.Archives
+
+Public Class Form1
+    Delegate Sub 写入日志框委托(text As String)
+
+    Private 客户端路径 As String
+    Private 差分包路径 As String
+    Private 语音差分包路径 As String
+
+    Private 差分包是否压缩包 As Boolean = False
+    Private 语音差分包是否压缩包 As Boolean = False
+
+    Private hpatchzExe As String
+
+    Private Sub 写入日志框(text As String)
+        If TextBox4.InvokeRequired Then
+            TextBox4.Invoke(New 写入日志框委托(AddressOf 写入日志框), text)
+        Else
+            TextBox4.AppendText(text & Environment.NewLine)
+            TextBox4.ScrollToCaret()
+        End If
+    End Sub
+
+    Private Sub 执行CMD(cmd As String)
+        Dim p As New Process()
+        p.StartInfo.FileName = cmd
+        p.StartInfo.RedirectStandardOutput = True
+        p.StartInfo.UseShellExecute = False
+        p.StartInfo.CreateNoWindow = True
+        p.Start()
+
+        Dim output As String = p.StandardOutput.ReadToEnd()
+        p.WaitForExit()
+        写入日志框(output)
+    End Sub
+
+    Private Sub 删除只读属性(path As String)
+        For Each file As String In Directory.GetFiles(path, "*", SearchOption.AllDirectories)
+            Dim fileInfo As New FileInfo(file)
+            fileInfo.Attributes = FileAttributes.Normal
+        Next
+    End Sub
+
+    Public Function 检查压缩包中的文件(zipFilePath As String, fileName As String) As Boolean
+        Try
+            Using archive As IArchive = ArchiveFactory.Open(zipFilePath)
+                For Each entry In archive.Entries
+                    If entry.Key.Equals(fileName, StringComparison.OrdinalIgnoreCase) Then
+                        Return True
+                    End If
+                Next
+            End Using
+        Catch ex As Exception
+            写入日志框("读取压缩包时出错：" & ex.Message)
+        End Try
+        Return False
+    End Function
+
+    Public Sub 解压压缩包(zipFilePath As String, extractTo As String)
+        Dim 压缩包 As IArchive = ArchiveFactory.Open(zipFilePath)
+        压缩包.ExtractToDirectory(extractTo)
+        压缩包.Dispose()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Not File.Exists("hpatchz.exe") Then
+            MessageBox.Show("hpatchz.exe 文件不存在于程序路径下！", "错误：", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        客户端路径 = TextBox2.Text
+        差分包路径 = TextBox1.Text
+        语音差分包路径 = TextBox3.Text
+        hpatchzExe = Path.Combine(Application.StartupPath, "hpatchz.exe")
+
+        If String.IsNullOrEmpty(客户端路径) OrElse String.IsNullOrEmpty(差分包路径) OrElse (CheckBox1.Checked AndAlso String.IsNullOrEmpty(语音差分包路径)) Then
+            MessageBox.Show("路径不能为空！", "错误：", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        If Not 差分包是否压缩包 Then
+            If Not File.Exists(Path.Combine(差分包路径, "deletefiles.txt")) OrElse Not File.Exists(Path.Combine(差分包路径, "hdifffiles.txt")) Then
+                MessageBox.Show("差分包文件不存在！", "错误：", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+        Else
+            If Not 检查压缩包中的文件(差分包路径, "hdifffiles.txt") OrElse Not 检查压缩包中的文件(差分包路径, "deletefiles.txt") Then
+                MessageBox.Show("差分包文件不存在或不正确！", "错误：", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+        End If
+
+        If Not 语音差分包是否压缩包 Then
+            If CheckBox1.Checked AndAlso Not File.Exists(Path.Combine(语音差分包路径, "hdifffiles.txt")) OrElse Not File.Exists(Path.Combine(差分包路径, "deletefiles.txt")) Then
+                MessageBox.Show("语音差分包文件不存在！", "错误：", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+        Else
+            If CheckBox1.Checked AndAlso Not 检查压缩包中的文件(语音差分包路径, "hdifffiles.txt") OrElse Not 检查压缩包中的文件(差分包路径, "deletefiles.txt") Then
+                MessageBox.Show("语音差分包文件不存在或不正确！", "错误：", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Return
+            End If
+        End If
+
+        If CheckBox1.Checked Then
+            Dim result As DialogResult = MessageBox.Show("请确认你所填的路径是否正确：" & vbCrLf & vbCrLf & "客户端路径：" & 客户端路径 & vbCrLf & "游戏差分包路径：" & 差分包路径 & vbCrLf & "语音差分包路径：" & 语音差分包路径 & vbCrLf & vbCrLf & "填写不正确的路径会导致合并失败，合并失败只能重新解压重来！", "警告：", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning)
+            If result = DialogResult.Cancel Then Return
+        Else
+            Dim result As DialogResult = MessageBox.Show("请确认你所填的路径是否正确：" & vbCrLf & vbCrLf & "客户端路径：" & 客户端路径 & vbCrLf & "游戏差分包路径：" & 差分包路径 & vbCrLf & vbCrLf & "填写不正确的路径会导致合并失败，合并失败只能重新解压重来！", "警告：", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning)
+            If result = DialogResult.Cancel Then Return
+        End If
+
+        Button1.Enabled = False
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If Not CheckBox1.Checked Then
+            Button4.Enabled = False
+            TextBox3.Enabled = False
+            Label3.Enabled = False
+            Button6.Enabled = False
+        End If
+
+        AddHandler BackgroundWorker1.DoWork, AddressOf BackgroundWorker1_DoWork
+        AddHandler BackgroundWorker1.RunWorkerCompleted, AddressOf BackgroundWorker1_RunWorkerCompleted
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs)
+        Try
+            If 差分包是否压缩包 Then
+                Dim 压缩文件目录 As String = Path.GetDirectoryName(差分包路径)
+                Dim 压缩文件名 As String = Path.GetFileNameWithoutExtension(差分包路径)
+                Dim 压缩文件路径 As String = 差分包路径
+                差分包路径 = 压缩文件目录 & “\” & 压缩文件名
+                If Directory.Exists(差分包路径) Then Directory.Delete(差分包路径, True)
+                写入日志框("解压：" & 压缩文件路径)
+                解压压缩包(压缩文件路径, 差分包路径)
+            End If
+
+            If CheckBox1.Checked AndAlso 语音差分包是否压缩包 Then
+                Dim 压缩文件目录 As String = Path.GetDirectoryName(语音差分包路径)
+                Dim 压缩文件名 As String = Path.GetFileNameWithoutExtension(语音差分包路径)
+                Dim 压缩文件路径 As String = 语音差分包路径
+                语音差分包路径 = 压缩文件目录 & “\” & 压缩文件名
+                If Directory.Exists(语音差分包路径) Then Directory.Delete(语音差分包路径, True)
+                写入日志框("解压：" & 压缩文件路径)
+                解压压缩包(压缩文件路径, 语音差分包路径)
+            End If
+
+            Dim deleteFiles As List(Of String) = File.ReadLines(Path.Combine(差分包路径, "deletefiles.txt")).ToList()
+            Dim deleteFilesAudio As List(Of String) = If(CheckBox1.Checked, File.ReadLines(Path.Combine(语音差分包路径, "deletefiles.txt")).ToList(), New List(Of String)())
+            Dim hdiffFiles As List(Of String) = File.ReadLines(Path.Combine(差分包路径, "hdifffiles.txt")).ToList()
+            Dim hdiffFilesAudio As List(Of String) = If(CheckBox1.Checked, File.ReadLines(Path.Combine(语音差分包路径, "hdifffiles.txt")).ToList(), New List(Of String)())
+
+            删除只读属性(客户端路径)
+            删除只读属性(差分包路径)
+            If CheckBox1.Checked Then 删除只读属性(语音差分包路径)
+
+            删除文件(客户端路径, deleteFiles)
+            If CheckBox1.Checked Then 删除文件(客户端路径, deleteFilesAudio)
+
+            Dim temp目录 As String = Path.Combine(客户端路径, "temp")
+            Directory.CreateDirectory(temp目录)
+
+            应用补丁(hdiffFiles, 客户端路径, 差分包路径, temp目录)
+            If CheckBox1.Checked Then 应用补丁(hdiffFilesAudio, 客户端路径, 语音差分包路径, temp目录)
+            移动文件(差分包路径, 客户端路径)
+            If CheckBox1.Checked Then 移动文件(语音差分包路径, 客户端路径)
+
+            Directory.Delete(差分包路径, True)
+            If CheckBox1.Checked Then Directory.Delete(语音差分包路径, True)
+
+            写入日志框("合并完成!")
+        Catch ex As Exception
+            写入日志框(ex.ToString())
+        End Try
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As System.ComponentModel.RunWorkerCompletedEventArgs)
+        Button1.Enabled = True
+
+        If e.Error IsNot Nothing Then
+            MessageBox.Show("合并过程中发生错误，请检查日志！", "错误：", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Else
+            MessageBox.Show("合并操作成功完成！", "信息：", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub 应用补丁(hdiffFiles As List(Of String), 客户端目录 As String, 差分包目录 As String, 临时目录 As String)
+        For Each line As String In hdiffFiles
+            Dim patchInfo As JsonObject = JsonSerializer.Deserialize(Of JsonObject)(line)
+            Dim remoteName As String = patchInfo("remoteName").ToString()
+            Dim baseName As String = Path.GetFileName(remoteName)
+
+            写入日志框($"合并：{baseName}...")
+
+            Dim 源文件 As String = Path.Combine(客户端目录, remoteName)
+            Dim hdiff文件 As String = Path.Combine(差分包目录, remoteName & ".hdiff")
+            Dim 目标文件 As String = Path.Combine(临时目录, baseName)
+
+            Dim cmd As String = $"""{hpatchzExe}"" ""{源文件}"" ""{hdiff文件}"" ""{目标文件}"""
+            执行CMD(cmd)
+
+            If File.Exists(源文件) Then File.Delete(源文件)
+            If File.Exists(hdiff文件) Then File.Delete(hdiff文件)
+            File.Move(目标文件, 源文件)
+        Next
+    End Sub
+
+    Private Sub 删除文件(文件路径 As String, 文件列表 As List(Of String))
+        For Each df As String In 文件列表
+            Dim filePath As String = Path.Combine(文件路径, df.Trim())
+            If File.Exists(filePath) Then
+                File.Delete(filePath)
+                写入日志框($"删除：{filePath}")
+            End If
+        Next
+    End Sub
+
+    Private Sub 移动文件(源路径 As String, 目标路径 As String)
+        For Each 源文件 As String In Directory.GetFiles(源路径, "*", SearchOption.AllDirectories)
+            Dim 相对路径 As String = 源文件.Substring(源路径.Length + 1)
+            Dim 目标文件 As String = Path.Combine(目标路径, 相对路径)
+            Dim 目的文件夹 As String = Path.GetDirectoryName(目标文件)
+            If Not Directory.Exists(目的文件夹) Then
+                Directory.CreateDirectory(目的文件夹)
+            End If
+            写入日志框($"移动：{源文件} -> {目标文件}...")
+            If File.Exists(目标文件) Then File.Delete(目标文件)
+            File.Move(源文件, 目标文件)
+        Next
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim fbd As New FolderBrowserDialog()
+        If fbd.ShowDialog() = DialogResult.OK Then
+            Dim fp As String = fbd.SelectedPath
+            TextBox2.Text = fp
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim fbd As New FolderBrowserDialog()
+        If fbd.ShowDialog() = DialogResult.OK Then
+            Dim fp As String = fbd.SelectedPath
+            差分包是否压缩包 = False
+            TextBox1.Text = fp
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim fbd As New FolderBrowserDialog()
+        If fbd.ShowDialog() = DialogResult.OK Then
+            Dim fp As String = fbd.SelectedPath
+            语音差分包是否压缩包 = False
+            TextBox3.Text = fp
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            Button4.Enabled = True
+            TextBox3.Enabled = True
+            Label3.Enabled = True
+            Button6.Enabled = True
+        Else
+            Button4.Enabled = False
+            TextBox3.Enabled = False
+            Label3.Enabled = False
+            Button6.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim ofd As New OpenFileDialog()
+        ofd.Multiselect = False
+        ofd.Filter = "压缩包文件 (*.zip) (*.7z)|*.zip;*.7z|Zip 文件 (*.zip)|*.zip|7Zip 文件 (*.7z)|*.7z|所有文件 (*.*)|*.*"
+
+        If ofd.ShowDialog() = DialogResult.OK Then
+            Dim fp As String = ofd.FileName
+            差分包是否压缩包 = True
+            TextBox1.Text = fp
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim ofd As New OpenFileDialog()
+        ofd.Multiselect = False
+        ofd.Filter = "压缩包文件 (*.zip) (*.7z)|*.zip;*.7z|Zip 文件 (*.zip)|*.zip|7Zip 文件 (*.7z)|*.7z|所有文件 (*.*)|*.*"
+
+        If ofd.ShowDialog() = DialogResult.OK Then
+            Dim fp As String = ofd.FileName
+            语音差分包是否压缩包 = True
+            TextBox3.Text = fp
+        End If
+    End Sub
+End Class
